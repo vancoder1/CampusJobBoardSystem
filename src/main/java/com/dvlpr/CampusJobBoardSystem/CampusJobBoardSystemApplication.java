dea489390835d9053d654a29afa461ca0aa9e6d0
vancoder1 <ivanzaporozhets25@gmail.com>
2025-11-25 16:35:30 -0700

    initialize Campus Job Board System with Maven setup and basic application structure

---
+++ src/main/java/com/dvlpr/CampusJobBoardSystem/CampusJobBoardSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dvlpr.CampusJobBoardSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusJobBoardSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusJobBoardSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dvlpr/CampusJobBoardSystem/CampusJobBoardSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvlpr.CampusJobBoardSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusJobBoardSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
